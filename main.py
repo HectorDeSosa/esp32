7270d1dd5a23efc6fab267df3379500493327a8a
HectorDeSosa <hectordesosa.01@gmail.com>
2024-07-09 19:11:34 -0300

    con wifi

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 import network
 import aioespnow
 import asyncio
+import uasyncio as asyncio
 import time
-
+import json
 def wifi_reset():   # Reset wifi to AP_IF off, STA_IF on and disconnected
   sta = network.WLAN(network.STA_IF); sta.active(False)
   sta.active(True)
@@ -20,9 +21,28 @@ while not sta.isconnected():  # Wait until connected...
 
 e = aioespnow.AIOESPNow()
 e.active(True)
+peer = b'0\xc9"2\xf6\xcc'   # MAC address of peer's wifi interface
+e.add_peer(peer)
 async def recv_till_halt(e):
-    async for mac, msg in e:
-        print(mac, msg)
-        if msg == b'halt':
-          break
-asyncio.run(recv_till_halt(e))
+    while True:
+        async for mac, msg in e:
+            print(mac, msg)
+            if msg == b'halt':
+                break
+        await asyncio.sleep(5)
+async def enviar(e,peer):
+    while True:
+        datos={'t':'topico','me':'prueba'}
+        await e.asend(peer, json.dumps(datos).encode('utf-8'), True)
+        print("enviado")
+        await asyncio.sleep(20)
+    
+async def main(e,peer):
+    task1 = asyncio.create_task(recv_till_halt(e))
+    task2 = asyncio.create_task(enviar(e, peer))
+    await asyncio.gather(task1, task2)
+    
+try:
+    asyncio.run(main(e,peer))
+finally:
+    asyncio.new_event_loop()
