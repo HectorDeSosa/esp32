1b4e47391557c41ee3781e508d3239dd34dfd116
HectorDeSosa <hectordesosa.01@gmail.com>
2024-07-09 18:47:26 -0300

    recibe_bien

--- main.py
+++ main.py
@@ -2,8 +2,22 @@
 import network
 import aioespnow
 import asyncio
-# A WLAN interface must be active to send()/recv()
-network.WLAN(network.STA_IF).active(True)
+import time
+
+def wifi_reset():   # Reset wifi to AP_IF off, STA_IF on and disconnected
+  sta = network.WLAN(network.STA_IF); sta.active(False)
+  sta.active(True)
+  while not sta.active():
+      time.sleep(0.1)
+  sta.disconnect()   # For ESP8266
+  return sta
+
+sta= wifi_reset()  # Reset wifi to AP off, STA on and disconnected
+sta.connect('Tenda_702298', '5KKfrLg9')
+sta.config(pm=sta.PM_NONE)
+while not sta.isconnected():  # Wait until connected...
+    time.sleep(0.1)
+
 e = aioespnow.AIOESPNow()
 e.active(True)
 async def recv_till_halt(e):
